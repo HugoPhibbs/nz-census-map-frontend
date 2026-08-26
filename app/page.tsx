c05066b1170d5549eef953be3daebcafd31a5b95
Hugo Phibbs <hugophibbs1@gmail.com>
2026-08-26 20:31:02 +0100

    unifying info box and map component with common state

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 "use client";
-import RegionalMap from './RegionalMap';
+import { Box } from '@mui/material';
+import RegionalMap from './components/RegionalMap';
 import {Map, Source, Layer} from '@vis.gl/react-maplibre';
 import {setWorkerUrl} from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css'; 
+import InfoBox from './components/InfoBox';
+import { useState } from 'react';
 
 setWorkerUrl('/maplibre/maplibre-gl-worker.mjs');
 
 export default function Home() {
-  return <RegionalMap></RegionalMap>
+const [chosenRegionId, setChosenRegionId] = useState<number | null>(null);
+
+  return (
+    <Box style={{ width: "100%", height: "100vh", display: "flex" }}>
+      <RegionalMap setChosenRegionId={setChosenRegionId} />
+      <InfoBox region_id={chosenRegionId} />
+    </Box>
+  )
 }
