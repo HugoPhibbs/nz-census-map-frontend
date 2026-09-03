42c9c4678fe0c6f89145a908dbbfa4e8204150ad
Hugo Phibbs <hugophibbs1@gmail.com>
2026-09-03 11:06:34 +0100

    adding in accordian deatils for demographic data

--- app/components/InfoPanel.tsx
+++ app/components/InfoPanel.tsx
@@ -1,16 +1,73 @@
 "use client";
 
-import { Box, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import api from "../api";
+import { roundToDP } from "../utils";
+
+const VARIABLE_GROUPS = {
+    "General": [
+        "pop_resident_usual",
+        "median_age",
+        "avg_children_born",
+    ],
+    "Employment": [
+        "median_personal_income",
+        "avg_hours_worked_per_week",
+    ],
+    "Ethnicities": [
+        "perc_ethnicity_pacific",
+        "perc_ethnicity_other",
+        "perc_ethnicity_mela",
+        "perc_ethnicity_maori",
+        "perc_ethnicity_european",
+        "perc_ethnicity_asian"
+    ],
+    "Birthplace": [
+        "perc_birthplace_nz",
+        "perc_birthplace_overseas"
+    ],
+}
+
+type GroupedVariablesProps = {
+    groupName: string;
+    groupVariableIds: string[];
+    areaVariables: Record<string, any> | null;
+    variableIdsToNameMap: Record<string, string>;
+};
+
+function GroupedVariables({ groupName, groupVariableIds, areaVariables, variableIdsToNameMap }: GroupedVariablesProps) {
+    return (
+        <Accordion>
+            <AccordionSummary>
+                <Typography>{groupName}</Typography>
+            </AccordionSummary>
+
+            <AccordionDetails>
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+                            {groupVariableIds.map((variableId) => (
+                                <TableRow key={variableId}>
+                                    <TableCell>{variableIdsToNameMap[variableId]}</TableCell>
+                                    <TableCell>{roundToDP(areaVariables?.[variableId].variable_value, 1)?? "N/A"}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </AccordionDetails>
+        </Accordion>
+    )
+}
 
 export default function InfoPanel({ areaId, variableIdsToNameMap }: { areaId: string | null; variableIdsToNameMap: Record<string, string> }) {
-    const [areaStats, setAreaStats] = useState<Record<string, any> | null>(null);
+    const [areaVariables, setAreaVariables] = useState<Record<string, any> | null>(null);
     const [areaName, setAreaName] = useState<string | null>(null);
 
     useEffect(() => {
         if (!areaId) {
-            setAreaStats(null);
+            setAreaVariables(null);
             setAreaName(null);
             return;
         }
@@ -26,7 +83,7 @@ export default function InfoPanel({ areaId, variableIdsToNameMap }: { areaId: st
     useEffect(() => {
         const areaIdSplit = areaId?.split("-") ?? null;
         if (!areaIdSplit) {
-            setAreaStats(null);
+            setAreaVariables(null);
             return;
         }
         console.assert(areaIdSplit.length === 2, "Area ID should be in the format 'census_year-area_code'");
@@ -36,20 +93,24 @@ export default function InfoPanel({ areaId, variableIdsToNameMap }: { areaId: st
         api.get("/stats/area",
             { "params": { "census_year": censusYear, "area_code": areaCode } })
             .then(res => {
-                const nextAreaStats: Record<string, any> = {};
+                const nextAreaVariables: Record<string, any> = {};
                 for (const row of res.data) {
-                    const variable_id = row.variable_id;
-                    const variableValue = row.variable_value;
-                    nextAreaStats[variable_id] = variableValue;
+                    nextAreaVariables[row.variable_id] = row;
                 }
-                setAreaStats(nextAreaStats);
+                setAreaVariables(nextAreaVariables);
             })
     }, [areaId])
 
     return <Box id={"info-box"}>
         <Typography component="h2">{areaName}</Typography>
-        {areaStats && Object.entries(areaStats).map(([variable_id, variableValue]) => (
-            <Typography key={variable_id}>{variableIdsToNameMap[variable_id]}: {variableValue}</Typography>
+        {Object.entries(VARIABLE_GROUPS).map(([groupName, groupVariableIds]) => (
+            <GroupedVariables
+                key={groupName}
+                groupName={groupName}   
+                groupVariableIds={groupVariableIds}
+                areaVariables={areaVariables}
+                variableIdsToNameMap={variableIdsToNameMap}
+            />
         ))}
     </Box>
 }
